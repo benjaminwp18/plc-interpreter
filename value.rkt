f46cf8583c4eb8073c7d5e2b53a08d52a4765140
kylekaufman <ktk33@case.edu>
2025-02-17 23:45:54 -0500

    reverting kyle mistake

--- value.rkt
+++ value.rkt
@@ -5,70 +5,79 @@
 (require "binding.rkt")
 
 ; value-int is a function that handles +, -, *, /, %, and the unary -
-(define (value-int expression)
+(define value-int
+  (lambda (expression state)
    (cond
-     [(number? expression) expression]
-     [(eq? '+ (operator expression)) (+         (value-generic(first-operand expression)) (value-generic(second-operand expression)))]
-     [(eq? '- (operator expression))            (helper-minus expression)]
-     [(eq? '* (operator expression)) (*         (value-generic(first-operand expression)) (value-generic(second-operand expression)))]
-     [(eq? '/ (operator expression)) (quotient  (value-generic(first-operand expression)) (value-generic(second-operand expression)))]
-     [(eq? '% (operator expression)) (remainder (value-generic(first-operand expression)) (value-generic(second-operand expression)))]
-     [else (error ' bad-op "Invalid Operator")]))
+     ((number? expression) expression)
+     ((eq? '+ (operator expression)) (+         (value-generic (first-operand expression) state) (value-generic (second-operand expression) state)))
+     ((eq? '- (operator expression))            (helper-minus expression state))
+     ((eq? '* (operator expression)) (*         (value-generic (first-operand expression) state) (value-generic (second-operand expression) state)))
+     ((eq? '/ (operator expression)) (quotient  (value-generic (first-operand expression) state) (value-generic (second-operand expression) state)))
+     ((eq? '% (operator expression)) (remainder (value-generic (first-operand expression) state) (value-generic (second-operand expression) state)))
+     (else (error ' bad-op "Invalid Operator")))))
 
 ; helper-minus is a helper function to make distinct operations for unary minus and subtraction. it checks if there is three elements in the list to perform subtraction, and does unary instead if there is only two elements
-(define (helper-minus expression)
-    (if (helper-is-binary expression) (- 0 (value-generic (first-operand expression)))
-        (-         (value-generic(first-operand expression)) (value-generic(second-operand expression)))))
+(define helper-minus
+  (lambda (expression state)
+    (if (helper-is-binary expression) (- 0 (value-generic (first-operand expression) state))
+        (-         (value-generic (first-operand expression) state) (value-generic (second-operand expression) state)))))
 
 ; helper-is-binary is a helper function to check if a list contains more than two elements
-(define (helper-is-binary expression)
+(define helper-is-binary
+  (lambda (expression)
     (if (null? (third-element-null-check expression)) #t
-        #f))
+        #f)))
 
 ; value-boolean is a function that handles ==, !=, >, <, <=, >=, &&, ||, !
-(define (value-boolean expression)
+(define value-boolean
+  (lambda (expression state)
     (cond
-      [(boolean? expression) expression]
-      [(eq? '== (operator expression)) (eq?         (value-generic(first-operand expression)) (value-generic(second-operand expression)))]
-      [(eq? '!= (operator expression)) (not(eq?     (value-generic(first-operand expression)) (value-generic(second-operand expression))))]
-      [(eq? '>  (operator expression)) (>           (value-generic(first-operand expression)) (value-generic(second-operand expression)))]
-      [(eq? '<  (operator expression)) (<           (value-generic(first-operand expression)) (value-generic(second-operand expression)))]
-      [(eq? '<= (operator expression)) (<=          (value-generic(first-operand expression)) (value-generic(second-operand expression)))]
-      [(eq? '>= (operator expression)) (>=          (value-generic(first-operand expression)) (value-generic(second-operand expression)))]
-      [(eq? '&& (operator expression)) (short-circuit-and expression)]
-      [(eq? '|| (operator expression)) (short-circuit-or  expression)]
-      [(eq? '!  (operator expression)) (not         (value-generic(first-operand expression)))]
-      [else (error ' bad-op "Invalid Operator")]))
+      ((boolean? expression) expression)
+      ((eq? '== (operator expression)) (eq?         (value-generic (first-operand expression) state) (value-generic (second-operand expression) state)))
+      ((eq? '!= (operator expression)) (not (eq?    (value-generic (first-operand expression) state) (value-generic (second-operand expression) state))))
+      ((eq? '>  (operator expression)) (>           (value-generic (first-operand expression) state) (value-generic (second-operand expression) state)))
+      ((eq? '<  (operator expression)) (<           (value-generic (first-operand expression) state) (value-generic (second-operand expression) state)))
+      ((eq? '<= (operator expression)) (<=          (value-generic (first-operand expression) state) (value-generic (second-operand expression) state)))
+      ((eq? '>= (operator expression)) (>=          (value-generic (first-operand expression) state) (value-generic (second-operand expression) state)))
+      ((eq? '&& (operator expression)) (short-circuit-and expression state))
+      ((eq? '|| (operator expression)) (short-circuit-or  expression state))
+      ((eq? '!  (operator expression)) (not         (value-generic (first-operand expression) state)))
+      (else (error ' bad-op "Invalid Operator")))))
 
 ; version of and function with explicit short circuiting
-(define (short-circuit-and expression)
+(define short-circuit-and
+  (lambda (expression state)
     (cond
-      [(eq? #f (value-generic(first-operand expression)))  #f]
-      [(eq? #f (value-generic(second-operand expression))) #f]
-      [else #t]))
+      ((eq? #f (value-generic (first-operand expression) state))  #f)
+      ((eq? #f (value-generic (second-operand expression) state)) #f)
+      (else #t))))
 
 ; version of or function with explicit short circuiting
-(define (short-circuit-or expression)
+(define short-circuit-or
+  (lambda (expression state)
     (cond
-      [(eq? #t (value-generic(first-operand expression)))  #t]
-      [(eq? #t (value-generic(second-operand expression))) #t]
-      [else #f]))
+      ((eq? #t (value-generic (first-operand expression) state))  #t)
+      ((eq? #t (value-generic (second-operand expression) state)) #t)
+      (else #f))))
 
 ; value-generic is a function to determine if an expression needs to be handled by value-boolean or value-int
-(define (value-generic expression)
+(define value-generic
+  (lambda (expression state)
     (cond
-      [(number? expression) (value-int expression)]
-      [(boolean? expression) (value-boolean expression)]
-      [(in-list? (operator expression) '(+ - * / %)) (value-int expression)]
-      [(in-list? (operator expression) '(== != > < <= >= && || !)) (value-boolean expression)]
-      [else (error ' bad-op "Invalid Operator")]))
+      ((number? expression) (value-int expression state))
+      ((boolean? expression) (value-boolean expression state))
+      ((eq? (binding-status expression state) binding-init) (binding-lookup expression state))
+      ((in-list? (operator expression) '(+ - * / %)) (value-int expression state))
+      ((in-list? (operator expression) '(== != > < <= >= && || !)) (value-boolean expression state))
+      (else (error ' bad-op "Invalid Operator")))))
 
 ; in-list takes an atom and a list and returns true if the element is in the list and false if the element is not in the list
-(define (in-list? a lis)
+(define in-list?
+  (lambda (a lis)
     (cond
-      [(null? lis)       #f]
-      [(eq? a (car lis)) #t]
-      [else             (in-list? a (cdr lis))]))
+      ((null? lis)       #f)
+      ((eq? a (car lis)) #t)
+      (else             (in-list? a (cdr lis))))))
 
 ;abstractions
 (define operator car)
