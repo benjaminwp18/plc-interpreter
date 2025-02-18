5a6fa8fcf2e52a459bbaab3a0450a43145b05e0c
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2025-02-18 14:35:10 -0500

    Working HTML tests

--- tester.rkt
+++ tester.rkt
@@ -5,16 +5,23 @@
 
 (require html-parsing)
 
+(provide (all-defined-out))
+
 (define (test-src)
   (map
    (lambda (str) (display (~a "Testing " str "...\n"
                               (with-handlers ([exn:fail? (lambda (v) (~a v "\n"))])
-                                (interpret (parser str)) "\n"))))
+                                (interpret (parser str))) "\n")))
    (list-files "tests/src")))
 
 (define (test-html)
   (map
-   (lambda (str) (display (~a "Testing " str "...\n" (interpret (parser str)) "\n")))
+   (lambda (filename)
+     (map
+      (lambda (str) (display (~a "Testing " str "...\n"
+                                 (with-handlers ([exn:fail? (lambda (v) (~a v "\n"))])
+                                   (interpret (parse-str str))) "\n\n")))
+      (parse-pres filename)))
    (list-files "tests/html")))
 
 (define (parse-pres filename)
@@ -23,8 +30,9 @@
 (define (find-pres xexp)
   (cond
     [(null? xexp) '()]
-    [(list? (car xexp)) ]
-    [(eq? (car xexp) 'pre)]))
+    [(list? (car xexp)) (append (find-pres (car xexp)) (find-pres (cdr xexp)))]
+    [(eq? (car xexp) 'pre) (list (string-join (cdr xexp) ""))]
+    [else (find-pres (cdr xexp))]))
 
 (define (parse-str str)
   (begin
