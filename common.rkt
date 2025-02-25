816b2626b2b83f00a09a2efdd6e8f629d0167e2d
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2025-02-25 14:51:21 -0500

    Refactor boolean/conditional ops

--- common.rkt
+++ common.rkt
@@ -1,12 +1,17 @@
 #lang racket
 
-(provide not-null? in-list?)
+(provide not-null? in-list? not-equal?)
 
+; returns true if a is not null; false otherwise
 (define (not-null? a) (not (null? a)))
 
-; in-list takes an atom and a list and returns true if the element is in the list and false if the element is not in the list
+; returns true if a is in lis; false otherwise
 (define (in-list? a lis)
     (cond
       [(null? lis)       #f]
       [(eq? a (car lis)) #t]
       [else             (in-list? a (cdr lis))]))
+
+; returns true if a does not equal b
+(define (not-equal? a b)
+  (not (equal? a b)))
