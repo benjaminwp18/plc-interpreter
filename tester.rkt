65f6fe037085ea754fb3d6f22f3de140f0ffcf73
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2025-02-25 12:32:34 -0500

    Comment & adjust func names

--- tester.rkt
+++ tester.rkt
@@ -7,31 +7,42 @@
 
 (provide test-src test-html)
 
+; Test all code files in the tests/src directory
 (define (test-src)
   (for ([filename (list-files "tests/src")])
     (display-test filename (parser filename))))
 
+; Test code from all <pre> tags in all HTML files in the tests/html directory
 (define (test-html)
   (for ([filename (list-files "tests/html")])
-    (for ([str (parse-pres filename)])
+    (for ([str (get-pre-contents-from-html filename)])
       (display-test str (parse-str str)))))
 
+; Display the results of a test
+; str is some identifier for the test
+; parsetree is the parse tree of the code to test
 (define (display-test str parsetree)
   (display (~a "======\n" str "\n---\nReturn value: "
                (with-handlers ([exn:fail? (lambda (v) (~a v "\n"))])
                  (interpret-tree parsetree))
                "\n======\n\n")))
 
-(define (parse-pres filename)
-  (find-pres (html->xexp (open-input-file filename))))
+; Convert an HTML file to a list of strings
+; s.t. each string is the contents of a <pre> tag
+(define (get-pre-contents-from-html filename)
+  (get-pre-contents (html->xexp (open-input-file filename))))
 
-(define (find-pres xexp)
+; Convert a xexp list to a list of strings
+; s.t. each string is the contents of a '(pre (...)) tag
+(define (get-pre-contents xexp)
   (cond
     [(null? xexp) '()]
-    [(list? (car xexp)) (append (find-pres (car xexp)) (find-pres (cdr xexp)))]
+    [(list? (car xexp)) (append (get-pre-contents (car xexp)) (get-pre-contents (cdr xexp)))]
     [(eq? (car xexp) 'pre) (list (string-join (cdr xexp) ""))]
-    [else (find-pres (cdr xexp))]))
+    [else (get-pre-contents (cdr xexp))]))
 
+; Get a parse tree from a code string
+; Uses temporary file tests/temp.j
 (define (parse-str str)
   (begin
     (with-output-to-file "tests/temp.j"
@@ -39,6 +50,7 @@
       #:exists 'replace)
     (parser "tests/temp.j")))
 
+; List the files in a directory
 (define (list-files dir)
   (map
    (lambda (path) (~a dir "/" (path->string path)))
