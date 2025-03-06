e5c554c8501649361a3180be65d66f9d6f470262
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2025-03-06 12:53:18 -0500

    Add layers to state

--- binding.rkt
+++ binding.rkt
@@ -3,71 +3,100 @@
 ;;;; ***************************************************
 ;;;; Kyle Kaufman, Benjamin Poulin, Kasey Wei
 ;;;; CSDS 345 Spring 2025
-;;;; Group Project 1: Simple Language Interpreter
+;;;; Group Project 2: Flow Control Interpreter
 ;;;; ***************************************************
 
 (provide binding-lookup binding-status binding-set binding-create
-         binding-unbound binding-uninit binding-init empty-stt stt-empty?)
+         binding-unbound binding-uninit binding-init empty-stt lyr-empty?)
+
+(define (binding-push-layer state)
+  (cons empty-lyr state))
+(define (binding-pop-layer state)
+  (stt-rest-lyrs state))
 
 ; Return bound value of name in state
 ; Error if binding does not exist
 (define (binding-lookup name state)
+  (if (stt-empty? state)
+      (error (~a name " has not been declared"))
+      (let ([result (lyr-lookup-binding name (stt-first-lyr state))])
+        (if (equal? binding-unbound result)
+            (binding-lookup name (stt-rest-lyrs state))
+            result))))
+
+(define (lyr-lookup-binding name layer)
   (cond
-    [(stt-empty? state) (error (~a name " has not been declared"))]
-    [(eq? (stt-first-name state) name) (stt-first-val state)]
-    [else (binding-lookup name (stt-cdr state))]))
+    [(lyr-empty? layer) binding-unbound]
+    [(equal? (lyr-first-name layer) name) (lyr-first-val layer)]
+    [else (lyr-lookup-binding name (lyr-cdr layer))]))
 
 ; Return binding-unbound if name's binding does not exist in state
 ; Return binding-uninit if it's not initialized
 ; Return binding-init otherwise
 (define (binding-status name state)
   (with-handlers ([exn:fail? (lambda (v) binding-unbound)])
-    (if (eq? (binding-lookup name state) binding-uninit)
+    (if (equal? (binding-lookup name state) binding-uninit)
         binding-uninit
         binding-init)))
+
+; Return state with value set for name's binding
+; Error if binding does not exist
+(define (binding-set name value state)
+  (if (stt-empty? state)
+      (error (~a name " has not been declared"))
+      (let ([result (lyr-set-binding name value (stt-first-lyr state))])
+        (if (equal? binding-unbound result)
+            (binding-set name value (stt-rest-lyrs state))
+            result))))
+
+(define (lyr-set-binding name value layer)
+  (cond
+    [(lyr-empty? layer) binding-unbound]
+    [(equal? (lyr-first-name layer) name) (lyr-cons (list name value) (lyr-cdr layer))]
+    [else (lyr-cons (lyr-car layer) (lyr-set-binding name value (lyr-cdr layer)))]))
+
+; Return state with new binding (name, value)
+; Error if binding already exists
+(define (binding-create name value state)
+  (if (equal? (binding-status name state) binding-unbound)
+      (lyr-create-binding name value (stt-first-lyr state))
+      (error (~a "Binding for " name " already exists"))))
+
+(define (lyr-create-binding name value layer)
+  (lyr-cons (list name value) layer))
 
 (define binding-unbound "unbound")
 (define binding-uninit '())
 (define binding-init "initialized")
 
-; Return state with value set for name's binding
-; Error if binding does not exist
-(define (binding-set name value state)
-  (cond
-    [(stt-empty? state) (error (~a name " has not been declared"))]
-    [(eq? (stt-first-name state) name) (stt-cons (list name value) (stt-cdr state))]
-    [else (stt-cons (stt-car state) (binding-set name value (stt-cdr state)))]))
+(define stt-first-lyr car)
+(define stt-rest-lyrs cdr)
 
-; Return state with new binding (name, value)
-; Error if binding already exists
-(define (binding-create name value state)
-  (cond
-    [(stt-empty? state) (stt-cons (list name value) empty-stt)]
-    [(eq? (stt-first-name state) name) (error (~a "Binding for " name " already exists"))]
-    [else (stt-cons (stt-car state) (binding-create name value (stt-cdr state)))]))
+(define empty-lyr '(() ()))
+(define empty-stt (list empty-lyr))
 
-(define empty-stt '(() ()))
-(define (stt-empty? state)
-  (or (null? (stt-names state))
-      (null? (stt-vals state))))
+(define (lyr-empty? layer)
+  (or (null? (lyr-names layer))
+      (null? (lyr-vals layer))))
+(define stt-empty? null?)
 
-(define stt-names car)
-(define stt-vals cadr)
-(define stt-name car)
-(define stt-val cadr)
-(define (stt-first-name state)
-  (car (stt-names state)))
-(define (stt-first-val state)
-  (car (stt-vals state)))
-(define (stt-car state)
+(define lyr-names car)
+(define lyr-vals cadr)
+(define binding-name car)
+(define binding-val cadr)
+(define (lyr-first-name layer)
+  (car (lyr-names layer)))
+(define (lyr-first-val layer)
+  (car (lyr-vals layer)))
+(define (lyr-car layer)
   (list
-   (car (stt-names state))
-   (car (stt-vals state))))
-(define (stt-cdr state)
+   (car (lyr-names layer))
+   (car (lyr-vals layer))))
+(define (lyr-cdr layer)
   (list
-   (cdr (stt-names state))
-   (cdr (stt-vals state))))
-(define (stt-cons pair state)
+   (cdr (lyr-names layer))
+   (cdr (lyr-vals layer))))
+(define (lyr-cons pair layer)
   (list
-   (cons (stt-name pair) (stt-names state))
-   (cons (stt-val pair) (stt-vals state))))
+   (cons (binding-name pair) (lyr-names layer))
+   (cons (binding-val pair) (lyr-vals layer))))
