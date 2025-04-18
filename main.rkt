fec35a86bd65e17b7bd79d352dc80209949efafa
kylekaufman <ktk33@case.edu>
2025-04-18 19:11:12 -0400

    switching to using closure methods

--- main.rkt
+++ main.rkt
@@ -19,23 +19,17 @@
   (state-first-pass-list
    tree
    empty-stt
-   (lambda (s)
-     (let* ((class-symbol (string->symbol classname))
-            (class-binding (lookup class-symbol s))) ;; step 1
-       (if (class-closure? class-binding)
-           (let* ((class-env (class-closure-env class-binding)) ; static env
-                  (main-binding (lookup 'main class-env)))       ; step 2
-             (if (method-closure? main-binding)
-                 (value-func-call
-                  '(main)                    ; step 3
-                  s
-                  (lambda (s) binding-uninit)
-                  identity
-                  (lambda (e s) (error (~a "Error during main method call: " e))))
-                 (error "Error: 'main' is not a method")))
-           (error (~a "Error: class " classname " not found")))))
+   (lambda (s) 
+     (define class-closure (create-class-closure classname s)) ;update once create class closure is added
+     (define method-closure (create-method-closure class-closure 'main)) ;update once create method closure is added
+     (define method-body  (closure-body method-closure))
+     (define method-scope ((closure-scope-func method-closure) s))
+     (define new-scope (binding-push-layer method-scope #t))
+     (state-statement-list method-body new-scope
+                          (lambda (s) binding-uninit)
+                          identity
+                          (lambda (e s) (error (~a "Error: " e)))))
    (lambda (e s) (error (~a "Error in global pass: " e)))))
-
 
 ; Perform the first pass (global scope) of tree
 ; Call next on the resulting state
